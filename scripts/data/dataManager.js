ac7a86ea839077c37a73734fc019223afd89d990
rushayhays <rushayhays@outlook.com>
2022-03-28 13:18:33 -0500

    The journal can now delete posts

--- scripts/data/dataManager.js
+++ scripts/data/dataManager.js
@@ -28,3 +28,27 @@ export const getPosts = () => {
         return parsedResponse;
     })
 }
+
+export const createPost = (postObj) => {
+    return fetch("http://localhost:8088/posts", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(postObj)
+  
+    })
+        .then(response => response.json())
+}
+
+export const deletePost = postId => {
+    return fetch(`http://localhost:8088/posts/${postId}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        }
+  
+    })
+        .then(response => response.json())
+        
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 import { EntryList } from "./journalEntryList.js";
 import { getLoggedInUser, getPosts } from "./data/dataManager.js";
+import { createPost, deletePost } from "./data/dataManager.js";
 
 // EntryList();
 console.log(getLoggedInUser());
@@ -41,19 +42,28 @@ applicationElement.addEventListener("click", (event) => {
 	}
 })
 
-//This detects a change in mood
+let journalEntryInProgress = {
+	date: "",
+	concept: "",
+	entry: "",
+	mood: ""
+};
+
+//This detects a change in date
 applicationElement.addEventListener("change", (event) => {
 	
 	if (event.target.id === "journalDate"){
 		console.log(event.target.value)
+		journalEntryInProgress.date = event.target.value;
 	}
 })
 
-//This detects a date
+//This detects a change in mood
 applicationElement.addEventListener("change", (event) => {
 	
 	if (event.target.id === "moodButton"){
 		console.log(event.target.value)
+		journalEntryInProgress.mood=event.target.value;
 	}
 })
 
@@ -62,6 +72,7 @@ applicationElement.addEventListener("keyup", (event) => {
 	
 	if (event.target.id === "textareaID"){
 		console.log(event.target.value)
+		journalEntryInProgress.entry = event.target.value
 	}
 })
 
@@ -69,14 +80,39 @@ applicationElement.addEventListener("keyup", (event) => {
 	
 	if (event.target.id === "subject"){
 		console.log(event.target.value)
+		journalEntryInProgress.concept = event.target.value;
 	}
 })
+
+// This clears the journal entry area
+const clearJournalEntryArea = () => {
+	let textArea = document.querySelector("#textareaID")
+	let subjectArea = document.querySelector("#subject")
+	let moodArea = document.querySelector("#moodButton")
+	let dateArea = document.querySelector("#journalDate")
+	textArea.value = "";
+	subjectArea.value = "";
+	moodArea.value = "";
+	dateArea.value = "";
+}
 
 //This notes if thr record button was pushed
 applicationElement.addEventListener("click", (event) => {
 	
 	if (event.target.id === "record"){
 		console.log("Recorded")
+		createPost(journalEntryInProgress).then(showPostList)
+		clearJournalEntryArea()
 	}
 })
 
+applicationElement.addEventListener("click", event => {
+	event.preventDefault();
+	if (event.target.id.startsWith("delete")) {
+	  const postId = event.target.id.split("--")[1];
+	  deletePost(postId)
+		.then(response => {
+		  showPostList();
+		})
+	}
+  })
